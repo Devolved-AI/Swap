be615a548ad0afdcc93fe667cbbac5f98bb8f3e6
Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>
2024-06-27 14:22:03 -0700

    Update wrap.js
    Updated wrap.js
    
    Signed-off-by: Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>

--- wrap.js
+++ wrap.js
@@ -1,28 +1,21 @@
 const { ethers } = require('ethers');
 
-async function wrap(provider, privateKey, nativeTokenAmount, wrappedTokenAddress) {
-  const signer = new ethers.Wallet(privateKey, provider);
-
+async function wrap(signer, nativeTokenAmount, wrappedTokenAddress) {
   const wrappedTokenContract = new ethers.Contract(
     wrappedTokenAddress,
     ['function deposit() payable', 'function withdraw(uint256) public'],
     signer
   );
 
-  try {
-    const tx = await wrappedTokenContract.deposit({ value: nativeTokenAmount });
-    const receipt = await tx.wait();
+  const tx = await wrappedTokenContract.deposit({ value: nativeTokenAmount });
+  const receipt = await tx.wait();
 
-    console.log('Wrapping successful');
-    console.log('Transaction Hash:', tx.hash);
-    console.log('Gas Used:', receipt.gasUsed.toString());
-    console.log('Block Hash:', receipt.blockHash);
-    console.log('Block Number:', receipt.blockNumber);
-    console.log('Timestamp:', (await provider.getBlock(receipt.blockNumber)).timestamp);
-  } catch (error) {
-    console.error('An error occurred during wrapping:', error);
-    throw error;
-  }
+  console.log('Wrapping successful');
+  console.log('Transaction Hash:', tx.hash);
+  console.log('Gas Used:', receipt.gasUsed.toString());
+  console.log('Block Hash:', receipt.blockHash);
+  console.log('Block Number:', receipt.blockNumber);
+  console.log('Timestamp:', (await signer.provider.getBlock(receipt.blockNumber)).timestamp);
 }
 
 module.exports = wrap;

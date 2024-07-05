8c9ed816517b8d9673112c725a8f2cb848f22eaf
Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>
2024-07-05 13:03:07 -0700

    Update AMM.sol
    Added more robust security features to smart contract
    
    Signed-off-by: Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>

--- AMM.sol
+++ AMM.sol
@@ -1,126 +1,163 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.22;
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract AMM {
-    IERC20 public token0;
-    IERC20 public token1;
-    uint public reserve0;
-    uint public reserve1;
-    uint public totalSupply;
-    mapping(address => uint) public balanceOf;
-    bool public initialized;
+contract AMM is ReentrancyGuard, Ownable, Pausable {
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+    uint256 public reserve0;
+    uint256 public reserve1;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
 
-    function initialize(address _token0, address _token1) external {
-        require(!initialized, "AMM already initialized");
+    uint256 private constant MINIMUM_LIQUIDITY = 1000;
+    uint256 private unlocked = 1;
+
+    event LiquidityAdded(address indexed provider, uint256 amount0, uint256 amount1, uint256 shares);
+    event LiquidityRemoved(address indexed provider, uint256 amount0, uint256 amount1, uint256 shares);
+    event Swap(address indexed user, address tokenIn, uint256 amountIn, uint256 amountOut);
+
+    constructor(address _token0, address _token1) Ownable(msg.sender) {
         require(_token0 != address(0) && _token1 != address(0), "Invalid token address");
         require(_token0 != _token1, "Tokens must be different");
         token0 = IERC20(_token0);
         token1 = IERC20(_token1);
-        initialized = true;
     }
 
-    modifier onlyInitialized() {
-        require(initialized, "AMM not initialized");
+
+    modifier lock() {
+        require(unlocked == 1, "AMM: LOCKED");
+        unlocked = 0;
         _;
+        unlocked = 1;
     }
 
-    function _mint(address _to, uint _amount) private {
+    function _mint(address _to, uint256 _amount) private {
         balanceOf[_to] += _amount;
         totalSupply += _amount;
     }
 
-    function _burn(address _from, uint _amount) private {
+    function _burn(address _from, uint256 _amount) private {
         balanceOf[_from] -= _amount;
         totalSupply -= _amount;
     }
 
-    function _update(uint _reserve0, uint _reserve1) private {
+    function _update(uint256 _reserve0, uint256 _reserve1) private {
         reserve0 = _reserve0;
         reserve1 = _reserve1;
     }
 
-    function swap(address _tokenIn, uint _amountIn)
+    function swap(address _tokenIn, uint256 _amountIn)
         external
-        onlyInitialized
-        returns (uint amountOut)
+        nonReentrant
+        lock
+        whenNotPaused
+        returns (uint256 amountOut)
     {
         require(
             _tokenIn == address(token0) || _tokenIn == address(token1),
-            "invalid token"
+            "Invalid token"
         );
-        require(_amountIn > 0, "amount in = 0");
-        bool isToken0 = _tokenIn == address(token0);
-        (
-            IERC20 tokenIn,
-            IERC20 tokenOut,
-            uint reserveIn,
-            uint reserveOut
-        ) = isToken0
-                ? (token0, token1, reserve0, reserve1)
-                : (token1, token0, reserve1, reserve0);
-        tokenIn.transferFrom(msg.sender, address(this), _amountIn);
+        require(_amountIn > 0, "Amount in = 0");
         
-        uint amountInWithFee = (_amountIn * 997) / 1000;
-        amountOut =
-            (reserveOut * amountInWithFee) /
-            (reserveIn + amountInWithFee);
+        bool isToken0 = _tokenIn == address(token0);
+        (IERC20 tokenIn, IERC20 tokenOut, uint256 reserveIn, uint256 reserveOut) = isToken0
+            ? (token0, token1, reserve0, reserve1)
+            : (token1, token0, reserve1, reserve0);
+
+        uint256 amountInWithFee = (_amountIn * 997) / 1000;
+        amountOut = (reserveOut * amountInWithFee) / (reserveIn + amountInWithFee);
+
+        require(amountOut > 0 && amountOut <= reserveOut, "Insufficient output amount");
+
+        tokenIn.transferFrom(msg.sender, address(this), _amountIn);
         tokenOut.transfer(msg.sender, amountOut);
+
         _update(
             token0.balanceOf(address(this)),
             token1.balanceOf(address(this))
         );
+
+        emit Swap(msg.sender, _tokenIn, _amountIn, amountOut);
     }
 
-    function addLiquidity(uint _amount0, uint _amount1)
+    function addLiquidity(uint256 _amount0, uint256 _amount1)
         external
-        onlyInitialized
-        returns (uint shares)
+        nonReentrant
+        lock
+        whenNotPaused
+        returns (uint256 shares)
     {
+        require(_amount0 > 0 && _amount1 > 0, "Amounts must be greater than 0");
+
+        uint256 balance0Before = token0.balanceOf(address(this));
+        uint256 balance1Before = token1.balanceOf(address(this));
+
         token0.transferFrom(msg.sender, address(this), _amount0);
         token1.transferFrom(msg.sender, address(this), _amount1);
-        if (reserve0 > 0 || reserve1 > 0) {
-            require(
-                reserve0 * _amount1 == reserve1 * _amount0,
-                "x / y != dx / dy"
-            );
-        }
+
+        uint256 balance0After = token0.balanceOf(address(this));
+        uint256 balance1After = token1.balanceOf(address(this));
+
+        uint256 amount0 = balance0After - balance0Before;
+        uint256 amount1 = balance1After - balance1Before;
+
         if (totalSupply == 0) {
-            shares = _sqrt(_amount0 * _amount1);
+            shares = _sqrt(amount0 * amount1);
+            require(shares > MINIMUM_LIQUIDITY, "Initial liquidity too low");
+            _mint(address(0), MINIMUM_LIQUIDITY); // Lock the minimum liquidity
+            shares -= MINIMUM_LIQUIDITY;
         } else {
             shares = _min(
-                (_amount0 * totalSupply) / reserve0,
-                (_amount1 * totalSupply) / reserve1
+                (amount0 * totalSupply) / reserve0,
+                (amount1 * totalSupply) / reserve1
             );
         }
-        require(shares > 0, "shares = 0");
+
+        require(shares > 0, "Shares = 0");
         _mint(msg.sender, shares);
-        _update(
-            token0.balanceOf(address(this)),
-            token1.balanceOf(address(this))
-        );
+
+        _update(balance0After, balance1After);
+
+        emit LiquidityAdded(msg.sender, amount0, amount1, shares);
     }
 
-    function removeLiquidity(uint _shares)
+
+    function removeLiquidity(uint256 _shares)
         external
-        onlyInitialized
-        returns (uint amount0, uint amount1)
+        nonReentrant
+        lock
+        whenNotPaused
+        returns (uint256 amount0, uint256 amount1)
     {
-        uint bal0 = token0.balanceOf(address(this));
-        uint bal1 = token1.balanceOf(address(this));
+        require(_shares > 0, "Shares must be greater than 0");
+        require(balanceOf[msg.sender] >= _shares, "Insufficient shares");
+
+        uint256 bal0 = token0.balanceOf(address(this));
+        uint256 bal1 = token1.balanceOf(address(this));
+
         amount0 = (_shares * bal0) / totalSupply;
         amount1 = (_shares * bal1) / totalSupply;
-        require(amount0 > 0 && amount1 > 0, "amount0 or amount1 = 0");
+
+        require(amount0 > 0 && amount1 > 0, "Insufficient liquidity");
+
         _burn(msg.sender, _shares);
         _update(bal0 - amount0, bal1 - amount1);
+
         token0.transfer(msg.sender, amount0);
         token1.transfer(msg.sender, amount1);
+
+        emit LiquidityRemoved(msg.sender, amount0, amount1, _shares);
     }
 
-    function _sqrt(uint y) private pure returns (uint z) {
+    function _sqrt(uint256 y) private pure returns (uint256 z) {
         if (y > 3) {
             z = y;
-            uint x = y / 2 + 1;
+            uint256 x = y / 2 + 1;
             while (x < z) {
                 z = x;
                 x = (y / x + x) / 2;
@@ -130,7 +167,15 @@ contract AMM {
         }
     }
 
-    function _min(uint x, uint y) private pure returns (uint) {
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
         return x <= y ? x : y;
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }

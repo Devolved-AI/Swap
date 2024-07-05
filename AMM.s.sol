f834bb28e98565a262d77fcebfe2dc97b35125be
Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>
2024-07-05 13:05:24 -0700

    Update AMM.s.sol
    Updated deployment script to accommodate new security features.
    
    Signed-off-by: Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>

--- AMM.s.sol
+++ AMM.s.sol
@@ -1,19 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.22;
 
 import "forge-std/Script.sol";
 import "../src/AMM.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DeployAMM is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
+        vm.startBroadcast();
 
-        // Deploy the AMM contract
-        AMM amm = new AMM();
-
-        console.log("AMM deployed at:", address(amm));
-        console.log("To initialize the AMM, call the initialize function with your chosen token addresses.");
+        // Create mock tokens
+        address token0 = address(0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238); // USDC Address - Sepolia
+        address token1 = address(0x8320a21f1088a97De8d2Aa288A715181Dd4142e3); // AGC Address - Sepolia
+        AMM amm = new AMM(token0, token1);
 
         vm.stopBroadcast();
     }

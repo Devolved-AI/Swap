afad6d53b3fa5d192b99125be771fb794d0183a3
Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>
2024-07-10 10:33:14 -0700

    Update AMM.s.sol
    Modified script to accommodate new wrap() and unwrap() functions.
    
    Signed-off-by: Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>

--- AMM.s.sol
+++ AMM.s.sol
@@ -3,16 +3,19 @@ pragma solidity ^0.8.22;
 
 import "forge-std/Script.sol";
 import "../src/AMM.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DeployAMM is Script {
     function run() external {
+        // Get the WETH address from the environment variable
+        address wethAddress = vm.envAddress("WETH_ADDRESS");
+        require(wethAddress != address(0), "WETH address not set");
+
         vm.startBroadcast();
 
-        // Create mock tokens
-        address token0 = address(0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238); // USDC Address - Sepolia
-        address token1 = address(0x8320a21f1088a97De8d2Aa288A715181Dd4142e3); // AGC Address - Sepolia
-        AMM amm = new AMM(token0, token1);
+        // Deploy AMM contract with WETH address
+        AMM amm = new AMM(wethAddress);
+
+        console.log("AMM deployed at:", address(amm));
 
         vm.stopBroadcast();
     }

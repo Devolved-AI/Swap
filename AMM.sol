832b04531965b80bca8a0db3eec8911010bacbae
Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>
2024-07-24 15:39:15 -0700

    Update AMM.sol
    Updated contract with more functions aimed at security
    
    Signed-off-by: Pavon Dunbar <36899956+pavondunbar@users.noreply.github.com>

--- AMM.sol
+++ AMM.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity 0.8.25;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
@@ -9,14 +10,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 interface IWETH {
     function deposit() external payable;
     function withdraw(uint256) external;
-    function balanceOf(address) external view returns (uint256);
-    function transfer(address, uint256) external returns (bool);
-    function transferFrom(address, address, uint256) external returns (bool);
-    function approve(address, uint256) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
-contract AMM is ReentrancyGuard, Pausable, Ownable { 
+contract AMM is ReentrancyGuard, Pausable, Ownable {
     using SafeERC20 for IERC20;
+    address public immutable wethAddress;
 
     struct LiquidityPair {
         IERC20 token0;
@@ -32,22 +32,49 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
         address token1;
     }
 
+    struct SwapInfo {
+        uint256 pairId;
+        bool isToken0;
+        uint256 reserveIn;
+        uint256 reserveOut;
+        uint256 amountIn;
+        uint256 amountOut;
+        uint256 protocolFee;
+    }
+
+    struct AddLiquidityParams {
+        uint256 pairId;
+        uint256 amount0;
+        uint256 amount1;
+        uint256 balance0Before;
+        uint256 balance1Before;
+        uint256 balance0After;
+        uint256 balance1After;
+    }
+
     mapping(uint256 => LiquidityPair) public liquidityPairs;
     mapping(uint256 => PairInfo) public pairInfo;
     mapping(address => mapping(address => uint256)) public getPairId;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
     uint256 public pairCount;
 
-    uint256 private constant MINIMUM_LIQUIDITY = 1000;
+    uint256 public constant MINIMUM_LIQUIDITY = 1;
     uint256 private unlocked = 1;
 
-    IWETH public weth;
+    uint256 public swapFee = 30; // 0.3% default fee (30 / 10000)
+    uint256 public constant LP_FEE_SHARE = 85; // 85% of the fee goes to LP
+    uint256 public accumulatedFees;
+
+    IWETH immutable public weth;
 
     event PairCreated(address indexed token0, address indexed token1, uint256 pairId);
     event LiquidityAdded(uint256 indexed pairId, address indexed provider, uint256 amount0, uint256 amount1, uint256 shares);
     event LiquidityRemoved(uint256 indexed pairId, address indexed provider, uint256 amount0, uint256 amount1, uint256 shares);
     event Swap(uint256 indexed pairId, address indexed user, address tokenIn, uint256 amountIn, uint256 amountOut);
-    event Wrap(address indexed user, uint256 amount);
-    event Unwrap(address indexed user, uint256 amount);
+    event FeesWithdrawn(address indexed owner, uint256 amount);
+    event SwapFeeUpdated(uint256 newFee);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     modifier lock() {
         require(unlocked == 1, "AMM: LOCKED");
@@ -56,12 +83,50 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
         unlocked = 1;
     }
 
-    constructor(address _weth) Ownable(msg.sender) {
-        require(_weth != address(0), "Invalid WETH address");
-        weth = IWETH(_weth);
+    constructor(address _wethAddress) Ownable(msg.sender) {
+        require(_wethAddress != address(0), "Invalid WETH address");
+        wethAddress = _wethAddress;
     }
 
-    function createPair(address _token0, address _token1) external onlyOwner returns (uint256 pairId) {
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender] - subtractedValue);
+        return true;
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal returns (bool) {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+        return true;
+    }
+
+    function setSwapFee(uint256 _swapFee) external onlyOwner {
+        require(_swapFee <= 100, "Fee cannot exceed 1%");
+        swapFee = _swapFee;
+        emit SwapFeeUpdated(_swapFee);
+    }
+
+    function wrap() external payable {
+        require(msg.value > 0, "Must send ETH to wrap");
+        IWETH(wethAddress).deposit{value: msg.value}();
+        require(IWETH(wethAddress).transfer(msg.sender, msg.value), "WETH transfer failed");
+    }
+
+    function unwrap(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
+        require(IWETH(wethAddress).transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        IWETH(wethAddress).withdraw(amount);
+        payable(msg.sender).transfer(amount);
+    }
+
+    function createPair(address _token0, address _token1) external whenNotPaused returns (uint256 pairId) {
         require(_token0 != address(0) && _token1 != address(0), "Invalid token address");
         require(_token0 != _token1, "Tokens must be different");
         require(getPairId[_token0][_token1] == 0, "Pair already exists");
@@ -71,6 +136,10 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
 
         liquidityPairs[pairId].token0 = IERC20(_token0);
         liquidityPairs[pairId].token1 = IERC20(_token1);
+
+        require(liquidityPairs[pairId].token0.totalSupply() > 0, "Token0 not ERC20");
+        require(liquidityPairs[pairId].token1.totalSupply() > 0, "Token1 not ERC20");
+
         pairInfo[pairId] = PairInfo({token0: _token0, token1: _token1});
         getPairId[_token0][_token1] = pairId;
         getPairId[_token1][_token0] = pairId;
@@ -84,6 +153,31 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
         return (_pairId, info.token0, info.token1);
     }
 
+    function getBalance(uint256 _pairId, address _account) public view returns (uint256) {
+        require(_pairId > 0 && _pairId <= pairCount, "Invalid pair ID");
+        return liquidityPairs[_pairId].balanceOf[_account];
+    }
+
+    function getAccumulatedFees() external view onlyOwner returns (uint256) {
+        return accumulatedFees;
+    }
+
+    function withdrawFees() external onlyOwner {
+        uint256 fees = accumulatedFees;
+        accumulatedFees = 0; // Update state variable first
+
+        // Emit event before making external call
+        emit FeesWithdrawn(owner(), fees);
+
+        // Make external call to transfer fees
+        IERC20(pairInfo[1].token0).safeTransfer(owner(), fees);
+    }
+
+    function _update(uint256 _pairId, uint256 _reserve0, uint256 _reserve1) internal {
+        liquidityPairs[_pairId].reserve0 = _reserve0;
+        liquidityPairs[_pairId].reserve1 = _reserve1;
+    }
+
     function addLiquidity(uint256 _pairId, uint256 _amount0, uint256 _amount1)
         external
         nonReentrant
@@ -92,40 +186,42 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
         returns (uint256 shares)
     {
         require(_pairId > 0 && _pairId <= pairCount, "Pair does not exist");
-        LiquidityPair storage pair = liquidityPairs[_pairId];
         require(_amount0 > 0 && _amount1 > 0, "Amounts must be greater than 0");
 
-        uint256 balance0Before = pair.token0.balanceOf(address(this));
-        uint256 balance1Before = pair.token1.balanceOf(address(this));
+        LiquidityPair storage pair = liquidityPairs[_pairId];
+        uint256 reserve0 = pair.reserve0;
+        uint256 reserve1 = pair.reserve1;
 
-        pair.token0.safeTransferFrom(msg.sender, address(this), _amount0);
-        pair.token1.safeTransferFrom(msg.sender, address(this), _amount1);
+        uint256 balance0After = reserve0 + _amount0;
+        uint256 balance1After = reserve1 + _amount1;
 
-        uint256 balance0After = pair.token0.balanceOf(address(this));
-        uint256 balance1After = pair.token1.balanceOf(address(this));
-
-        uint256 amount0 = balance0After - balance0Before;
-        uint256 amount1 = balance1After - balance1Before;
-
+        // Calculate shares
         if (pair.totalSupply == 0) {
-            shares = _sqrt(amount0 * amount1);
+            shares = _sqrt(_amount0 * _amount1);
             require(shares > MINIMUM_LIQUIDITY, "Initial liquidity too low");
             _mint(_pairId, address(0), MINIMUM_LIQUIDITY);
             shares -= MINIMUM_LIQUIDITY;
         } else {
             shares = _min(
-                (amount0 * pair.totalSupply) / pair.reserve0,
-                (amount1 * pair.totalSupply) / pair.reserve1
+                (_amount0 * pair.totalSupply) / reserve0,
+                (_amount1 * pair.totalSupply) / reserve1
             );
         }
 
         require(shares > 0, "Shares = 0");
+
+        // Update state variables
+        _update(_pairId, balance0After, balance1After);
         _mint(_pairId, msg.sender, shares);
 
-        _update(_pairId, balance0After, balance1After);
+        // Transfer tokens to contract
+        pair.token0.safeTransferFrom(msg.sender, address(this), _amount0);
+        pair.token1.safeTransferFrom(msg.sender, address(this), _amount1);
 
-        emit LiquidityAdded(_pairId, msg.sender, amount0, amount1, shares);
-    }
+        emit LiquidityAdded(_pairId, msg.sender, _amount0, _amount1, shares);
+
+        return shares;
+    }   
 
     function removeLiquidity(uint256 _pairId, uint256 _shares)
         external
@@ -139,24 +235,71 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
         require(_shares > 0, "Shares must be greater than 0");
         require(pair.balanceOf[msg.sender] >= _shares, "Insufficient shares");
 
-        uint256 bal0 = pair.token0.balanceOf(address(this));
-        uint256 bal1 = pair.token1.balanceOf(address(this));
+        uint256 reserve0 = pair.reserve0;
+        uint256 reserve1 = pair.reserve1;
 
-        amount0 = (_shares * bal0) / pair.totalSupply;
-        amount1 = (_shares * bal1) / pair.totalSupply;
+        amount0 = (_shares * reserve0) / pair.totalSupply;
+        amount1 = (_shares * reserve1) / pair.totalSupply;
 
         require(amount0 > 0 && amount1 > 0, "Insufficient liquidity");
 
         _burn(_pairId, msg.sender, _shares);
-        _update(_pairId, bal0 - amount0, bal1 - amount1);
+        _update(_pairId, reserve0 - amount0, reserve1 - amount1);
 
         pair.token0.safeTransfer(msg.sender, amount0);
         pair.token1.safeTransfer(msg.sender, amount1);
 
         emit LiquidityRemoved(_pairId, msg.sender, amount0, amount1, _shares);
+    } 
+    
+    function getReserve0(uint256 pairId) public view returns (uint256) {
+        return liquidityPairs[pairId].reserve0;
+    }
+     
+    function getReserve1(uint256 pairId) public view returns (uint256) {
+        return liquidityPairs[pairId].reserve1;
     }
 
-    function swap(uint256 _pairId, address _tokenIn, uint256 _amountIn)
+    function getTotalSupply(uint256 pairId) public view returns (uint256) {
+        return liquidityPairs[pairId].totalSupply;
+    }
+
+    function _transferTokensToContract(AddLiquidityParams memory params) internal {
+        liquidityPairs[params.pairId].token0.safeTransferFrom(msg.sender, address(this), params.amount0);
+        liquidityPairs[params.pairId].token1.safeTransferFrom(msg.sender, address(this), params.amount1);
+    }
+
+    function _calculateAndMintShares(AddLiquidityParams memory params) internal returns (uint256 shares) {
+        LiquidityPair storage pair = liquidityPairs[params.pairId];
+        uint256 amount0 = params.balance0After - params.balance0Before;
+        uint256 amount1 = params.balance1After - params.balance1Before;
+
+        require(amount0 <= type(uint256).max / pair.totalSupply, "Overflow");
+        require(amount1 <= type(uint256).max / pair.totalSupply, "Overflow");
+
+        if (pair.totalSupply == 0) {
+            shares = _sqrt(amount0 * amount1);
+            require(shares > MINIMUM_LIQUIDITY, "Initial liquidity too low");
+            _mint(params.pairId, address(0), MINIMUM_LIQUIDITY);
+            shares -= MINIMUM_LIQUIDITY;
+        } else {
+            shares = _min(
+                (amount0 * pair.totalSupply) / pair.reserve0,
+                (amount1 * pair.totalSupply) / pair.reserve1
+            );
+        }
+
+        require(shares > 0, "Shares = 0");
+        _mint(params.pairId, msg.sender, shares);
+        return shares;
+    }
+
+    function swap(
+        uint256 _pairId,
+        address _tokenIn,
+        uint256 _amountIn,
+        uint256 _minAmountOut
+    )
         external
         nonReentrant
         lock
@@ -170,48 +313,66 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
             "Invalid token"
         );
         require(_amountIn > 0, "Amount in = 0");
-        
+
         bool isToken0 = _tokenIn == address(pair.token0);
-        (IERC20 tokenIn, IERC20 tokenOut, uint256 reserveIn, uint256 reserveOut) = isToken0
-            ? (pair.token0, pair.token1, pair.reserve0, pair.reserve1)
-            : (pair.token1, pair.token0, pair.reserve1, pair.reserve0);
+        uint256 reserveIn = isToken0 ? pair.reserve0 : pair.reserve1;
+        uint256 reserveOut = isToken0 ? pair.reserve1 : pair.reserve0;
 
-        uint256 amountInWithFee = (_amountIn * 997) / 1000;
-        amountOut = (reserveOut * amountInWithFee) / (reserveIn + amountInWithFee);
+        uint256 amountInWithFee = _amountIn * (10000 - swapFee);
+        amountOut = (amountInWithFee * reserveOut) / ((reserveIn * 10000) + amountInWithFee);
 
-        require(amountOut > 0 && amountOut <= reserveOut, "Insufficient output amount");
+        require(amountOut >= _minAmountOut, "Insufficient output amount");
 
-        tokenIn.safeTransferFrom(msg.sender, address(this), _amountIn);
-        tokenOut.safeTransfer(msg.sender, amountOut);
+        // Update reserves
+        uint256 newReserveIn = reserveIn + _amountIn;
+        uint256 newReserveOut = reserveOut - amountOut;
+        _update(_pairId, isToken0 ? newReserveIn : newReserveOut, isToken0 ? newReserveOut : newReserveIn);
 
-        _update(
-            _pairId,
-            pair.token0.balanceOf(address(this)),
-            pair.token1.balanceOf(address(this))
-        );
+        // Update accumulated fees
+        uint256 fee = (_amountIn * swapFee) / 10000;
+        accumulatedFees += fee;
+
+        // Transfer tokens
+        IERC20(isToken0 ? pair.token0 : pair.token1).safeTransferFrom(msg.sender, address(this), _amountIn);
+        IERC20(isToken0 ? pair.token1 : pair.token0).safeTransfer(msg.sender, amountOut);
 
         emit Swap(_pairId, msg.sender, _tokenIn, _amountIn, amountOut);
+
+        return amountOut;
     }
 
-    function wrap() external payable nonReentrant whenNotPaused {
-        require(msg.value > 0, "Must send ETH to wrap");
-        weth.deposit{value: msg.value}();
-        require(weth.transfer(msg.sender, msg.value), "WETH transfer failed");
-        emit Wrap(msg.sender, msg.value);
+    function _prepareSwapInfo(uint256 _pairId, address _tokenIn, uint256 _amountIn) internal view returns (SwapInfo memory) {
+        LiquidityPair storage pair = liquidityPairs[_pairId];
+        bool isToken0 = _tokenIn == address(pair.token0);
+        
+        return SwapInfo({
+            pairId: _pairId,
+            isToken0: isToken0,
+            reserveIn: isToken0 ? pair.reserve0 : pair.reserve1,
+            reserveOut: isToken0 ? pair.reserve1 : pair.reserve0,
+            amountIn: _amountIn,
+            amountOut: 0,
+            protocolFee: (_amountIn * swapFee) / 10000
+        });
     }
 
-    function unwrap(uint256 _amount) external nonReentrant whenNotPaused {
-        require(_amount > 0, "Amount must be greater than 0");
-        IERC20(address(weth)).safeTransferFrom(msg.sender, address(this), _amount);
-        weth.withdraw(_amount);
-        (bool success, ) = msg.sender.call{value: _amount}("");
-        require(success, "ETH transfer failed");
-        emit Unwrap(msg.sender, _amount);
-    }
+    function _calculateAmountOut(SwapInfo memory swapInfo) internal pure returns (uint256) {
+        uint256 numerator = swapInfo.amountIn * (10000 - swapInfo.protocolFee) * swapInfo.reserveOut;
+        uint256 denominator = (swapInfo.reserveIn * 10000) + (swapInfo.amountIn * (10000 - swapInfo.protocolFee));
+        return numerator / denominator;
+    }    
 
-    function getBalance(uint256 _pairId, address _account) public view returns (uint256) {
-        require(_pairId > 0 && _pairId <= pairCount, "Invalid pair ID");
-        return liquidityPairs[_pairId].balanceOf[_account];
+    function _updateReserves(SwapInfo memory swapInfo) private {
+        uint256 newReserveIn = swapInfo.reserveIn + swapInfo.amountIn;
+        uint256 newReserveOut = swapInfo.reserveOut - swapInfo.amountOut;
+
+        _update(
+            swapInfo.pairId,
+            swapInfo.isToken0 ? newReserveIn : newReserveOut,
+            swapInfo.isToken0 ? newReserveOut : newReserveIn
+        );
+
+        accumulatedFees += swapInfo.protocolFee;
     }
 
     function _mint(uint256 _pairId, address _to, uint256 _amount) private {
@@ -226,13 +387,7 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
         pair.totalSupply -= _amount;
     }
 
-    function _update(uint256 _pairId, uint256 _reserve0, uint256 _reserve1) private {
-        LiquidityPair storage pair = liquidityPairs[_pairId];
-        pair.reserve0 = _reserve0;
-        pair.reserve1 = _reserve1;
-    }
-
-    function _sqrt(uint256 y) private pure returns (uint256 z) {
+    function _sqrt(uint256 y) internal pure returns (uint256 z) {
         if (y > 3) {
             z = y;
             uint256 x = y / 2 + 1;
@@ -245,7 +400,7 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
         }
     }
 
-    function _min(uint256 x, uint256 y) private pure returns (uint256) {
+    function _min(uint256 x, uint256 y) internal pure returns (uint256) {
         return x <= y ? x : y;
     }
 
@@ -257,5 +412,11 @@ contract AMM is ReentrancyGuard, Pausable, Ownable {
         _unpause();
     }
 
-    receive() external payable {}
+    receive() external payable {
+        revert("Contract does not accept ETH directly");
+    }
+
+    function getUnlocked() external view returns (uint256) {
+        return unlocked;
+    }
 }
